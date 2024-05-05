8f80e77804ff6ea90964cfe8a027bac48c4145c1
michang <minseok128128@gmail.com>
2024-05-05 14:38:15 +0900

    Update newFlask : 구현 성공

--- newFlaskTest.py
+++ newFlaskTest.py
@@ -1,26 +1,36 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session
 import time
+import os
 import dm_ai
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def index():
+    session.pop('chat', None)
+    session['chat'] = session.get('chat', conversation_history)
     return render_template('newIndex.html')
 
 @app.route('/submit_form', methods=['POST'])
 def submit_form():
-    user_info = request.form['user']
-    message_info = request.form['message']
-    
-    time.sleep(3)
-    
-    print("Received user name:", user_info, " | Received message:", message_info)
+    global client
+
+    user = request.form['user']
+    message = request.form['message']
+
+    session.modified = True
+    response_message = dm_ai.generate_chat(client, message, session['chat'])
     response_data = {
         'status': 'success',
-        'message': 'Form data received correctly!'
+        'message': response_message
     }
+    print("Chat history:", session['chat'])
     return jsonify(response_data)
 
 if __name__ == '__main__':
+    client = dm_ai.create_openai_client()
+    conversation_history = [{"role": "system", "content": dm_ai.system_prompt}]
+    dm_ai.system_token = dm_ai.num_tokens_from_messages(conversation_history, model=dm_ai.MODEL)
+    dm_ai.encoding = dm_ai.tiktoken.encoding_for_model(dm_ai.MODEL)
     app.run(debug=True)
