50bd6e817997f712b596f7677f3897ab2332a9b3
michang <minseok128128@gmail.com>
2024-05-05 12:59:15 +0900

    Added newFlaskTest.py : for post test

---
+++ newFlaskTest.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('newIndex.html')
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    user_info = request.form['user']
+    message_info = request.form['message']
+    
+    print("Received user name:", user_info, " | Received message:", message_info)
+    response_data = {
+        'status': 'success',
+        'message': 'Form data received correctly!'
+    }
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
